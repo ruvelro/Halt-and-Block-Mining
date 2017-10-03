32c226d43d16001c482766b5c648f90c04ca473c
ruvelro <ruvelro@gmail.com>
2017-10-03 08:40:29 +0200

    Update HBmining.bat
    Added new mining hosts

--- HBmining.bat
+++ HBmining.bat
@@ -4,6 +4,16 @@ set hostspath=%windir%\System32\drivers\etc\hosts
 
 echo #[Halt and Block Mining by RedesZone] >> %hostspath%
 echo 0.0.0.0 coin-hive.com >> %hostspath%
+echo 0.0.0.0 cnhv.co >> %hostspath%
+echo 0.0.0.0 gus.host >> %hostspath%
+echo 0.0.0.0 jsecoin.com >> %hostspath%
+echo 0.0.0.0 miner.pr0gramm.com >> %hostspath%
+echo 0.0.0.0 jsecoin.com >> %hostspath%
+echo 0.0.0.0 reasedoper.pw >> %hostspath%
+echo 0.0.0.0 mataharirama.xyz >> %hostspath%
+echo 0.0.0.0 listat.biz >> %hostspath%
+echo 0.0.0.0 lmodr.biz >> %hostspath%
+echo 0.0.0.0 minecrunch.co >> %hostspath%
 
 echo ipconfig /flushdns
 
